4caf6436fa7d7a4d5d171367c7f75b28ba27de30
Winndie Fan <winndie@gmail.com>
2023-09-04 13:35:48 +0100

    Added unit tests

---
+++ Tests/PokedexTests.cs
@@ -0,0 +1,27 @@
+﻿using Pokedex.Services;
+using Xunit;
+
+namespace Pokedex.Tests
+{
+    public class PokedexTests
+    {
+        [Fact]
+        public void GetEvolutionChain_For_NotAPokemon_Returns_Fail()
+        {
+            var pokedexService = new PokedexService();
+            var result = pokedexService.GetEvolutionChain("Not a Pokemon");
+
+            Assert.False(result.IsSuccess, "Not a Pokemon");
+        }
+
+        [Fact]
+        public void GetEvolutionChain_For_Charmeleon_Returns_Success()
+        {
+            var pokedexService = new PokedexService();
+            var result = pokedexService.GetEvolutionChain("Charmeleon");
+
+            Assert.True(result.IsSuccess, "Charmeleon");
+            Assert.Equal(1, result.Data.Variations.Count());
+        }
+    }
+}
